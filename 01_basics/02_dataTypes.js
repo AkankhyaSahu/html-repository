c6b2ad50051d7af7173fe67aa78556c27c01488c
AkankhyaSahu <sahuakankhya7@gmail.com>
2025-12-08 07:27:24 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Akankhya"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint for long numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniqueness
+
+//object 
+
+console.log(typeof "akankhya") // string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
